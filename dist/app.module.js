a4639721d06f1d5619d5006b42aef48c35b16719
mobin.shaterain <mobinest@gmail.com>
2022-01-23 10:20:25 +0330

    modules update

--- dist/app.module.js
+++ dist/app.module.js
@@ -10,15 +10,14 @@ exports.AppModule = void 0;
 const common_1 = require("@nestjs/common");
 const app_controller_1 = require("./app.controller");
 const app_service_1 = require("./app.service");
-const coffees_controller_1 = require("./coffees/coffees.controller");
-const coffees_service_1 = require("./coffees/coffees.service");
+const coffees_module_1 = require("./coffees/coffees.module");
 let AppModule = class AppModule {
 };
 AppModule = __decorate([
     (0, common_1.Module)({
-        imports: [],
-        controllers: [app_controller_1.AppController, coffees_controller_1.CoffeesController],
-        providers: [app_service_1.AppService, coffees_service_1.CoffeesService],
+        imports: [coffees_module_1.CoffeesModule],
+        controllers: [app_controller_1.AppController],
+        providers: [app_service_1.AppService],
     })
 ], AppModule);
 exports.AppModule = AppModule;
